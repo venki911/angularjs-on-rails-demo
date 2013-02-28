bd67206419bfdf41e368432482e4bf0515020328
sonnym <michaud.sonny@gmail.com>
2013-02-28 10:08:04 -0500

    add csrf token header to all ajax requests
    This step is an essential prerequisite to being able to communicate with
    Rails in any meaningful (i.e. not idempotent) way. In doing so, two
    important items are exposed.  The former is the #csrf_meta_tag from
    Rails, which creates an authenticity token meta tag.  The latter is the
    ability to configure the $httpProvider service from the primary
    application module.

--- app/assets/javascripts/chatter.js
+++ app/assets/javascripts/chatter.js
@@ -1,5 +1,8 @@
-var chatter_module = angular.module("chatter", []);
+var chatter_module = angular.module("chatter", ["ng"]);
 
-chatter_module.config(function($routeProvider) {
+chatter_module.config(function($routeProvider, $httpProvider) {
   $routeProvider.when("/", { controller: SessionCtrl, templateUrl: "assets/sessions/new.html" });
+
+  var token = angular.element(document.querySelector("meta[name='csrf-token']")).attr("content");
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] = token;
 });
